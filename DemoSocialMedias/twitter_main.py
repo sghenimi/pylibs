40c4cfc1941b44c7af8eb7995b834781a6de5891
sghenimi <soufiane.ghenimi@gmail.com>
2022-07-12 06:15:05 +0200

    more example from twitter

--- DemoSocialMedias/twitter_main.py
+++ DemoSocialMedias/twitter_main.py
@@ -12,25 +12,21 @@ api_key_secret = config['twitter']['api_key_secret']
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
+#v_bearer_token = config['twitter']['bearer_token']
+#print(v_bearer_token)
 # authentication
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-api.update_status("hello via tweepy")
-
-# public_tweets = api.get_retweets()
-#
-# for twt in public_tweets[:2]:
-#     print(100*'-')
-#     print(twt)
+public_tweets = api.home_timeline()
 
 # create dataframe
-# columns = ['Time', 'User', 'Tweet']
-# data = []
-# for tweet in public_tweets:
-#     data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
-#
-# df = pd.DataFrame(data, columns=columns)
-#
-# df.to_csv('tweets_get_friends.csv')
+columns = ['Time', 'User', 'Tweet']
+data = []
+for tweet in public_tweets:
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
+
+df = pd.DataFrame(data, columns=columns)
+
+df.to_csv('tweets_home_timeline.csv', index=False)
